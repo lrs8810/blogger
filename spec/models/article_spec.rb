64b474f213490a5e1973414d4ee975a7d844f364
Laura Schulz <lrs8810@gmail.com>
2019-10-02 21:32:35 -0600

    Add article model

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe Article, type: :model do
   describe "validations" do
-    it {should validate_presence of(:title)}
-    it {should validate_presence of(:body)}
+    it {should validate_presence_of(:title)}
+    it {should validate_presence_of(:body)}
   end
 end
